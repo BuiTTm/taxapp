84552004110cbecd2dd30aea14760c4a660a17b2
alexhalawi <alexandrehalawi@gmail.com>
2022-04-19 01:46:49 -0400

    Update settings.py

--- taxapp/settings.py
+++ taxapp/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 #DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://mgl7760.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com']
 
 
 
